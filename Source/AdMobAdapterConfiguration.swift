c03b942743fc14d5ff54c26803b1234bd76e2ac4
Daniel Barros <daniel.barros@chartboost.com>
2022-11-29 16:59:29 +0100

    Make setTestDeivceID public so it can be used from our test app

--- Source/AdMobAdapterConfiguration.swift
+++ Source/AdMobAdapterConfiguration.swift
@@ -11,7 +11,7 @@ import GoogleMobileAds
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
 public class AdMobAdapterConfiguration {
     /// Google's identifier for your test device can be found in the console output from their SDK
-    class func setTestDeviceId(_ id: String?) {
+    public static func setTestDeviceID(_ id: String?) {
         if let id = id {
             GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [id]
         } else {
