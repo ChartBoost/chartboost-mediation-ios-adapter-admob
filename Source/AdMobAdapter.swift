8e050164565b24908939fad1397567c4f76ed33e
Alex Rice <rice.alexanderb@gmail.com>
2023-01-10 13:38:51 -0800

    Make adapterVersion a hard-coded string

--- Source/AdMobAdapter.swift
+++ Source/AdMobAdapter.swift
@@ -25,7 +25,7 @@ final class AdMobAdapter: PartnerAdapter {
     /// The version of the adapter.
     /// It should have either 5 or 6 digits separated by periods, where the first digit is Helium SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
     /// Format: `<Helium major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
-    var adapterVersion = "4.\(getGADVersionString()).0"
+    var adapterVersion = "4.9.12.0.0"
     
     /// The partner's unique identifier.
     var partnerIdentifier = "admob"
