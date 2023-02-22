f361eb4384396821a2426a8375bcd674e2b254e4
Daniel Barros <daniel.barros@chartboost.com>
2023-02-22 16:02:14 +0100

    Add release workflow, clean up scripts

---
+++ Scripts/adapter-version.rb
@@ -0,0 +1,4 @@
+require_relative 'common'
+
+# Take podspec version as the adapter version and output to console
+puts podspec_version
--- Scripts/next-rc-version.rb
+++ Scripts/next-rc-version.rb
@@ -1,14 +1,7 @@
-PODSPEC_PATH_PATTERN = "*.podspec"
-PODSPEC_VERSION_REGEX = /^\s*spec\.version\s*=\s*'([0-9]+.[0-9]+.[0-9]+.[0-9]+.[0-9]+(?>.[0-9]+)?)'\s*$/
-
-# Obtain the podspec file path
-file = Dir.glob(PODSPEC_PATH_PATTERN).first
-fail unless !file.nil?
+require_relative 'common'
 
 # Obtain the adapter version from the podspec
-text = File.read(file)
-version = text.match(PODSPEC_VERSION_REGEX).captures.first
-fail unless !version.nil?
+version = podspec_version()
 
 # Generate the filter pattern
 filter_pattern = "#{version}-rc."
