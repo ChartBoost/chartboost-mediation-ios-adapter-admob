7bd9ea75f8d47277074ab6b11aca6179b79a7e8c
Daniel Barros <daniel.barros@chartboost.com>
2023-04-26 11:14:15 -0700

    Remove scripts and update workflows to use actions in external repo (#51)

--- Scripts/validate-adapter-version.rb
+++
@@ -1,4 +0,0 @@
-require_relative 'common'
-
-# Fail if versions in podpsec and in the main adapter class don't match
-abort "Validation failed: #{podspec_version} != #{adapter_class_version}." unless podspec_version == adapter_class_version
