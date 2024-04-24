8328cdda7fc982f4686fc29017414a909c49a2f6
Daniel Barros <daniel.barros@chartboost.com>
2024-04-24 15:29:46 -0700

    Relocate adapter properties to configuration

--- Source/AdMobAdapterConfiguration.swift
+++ Source/AdMobAdapterConfiguration.swift
@@ -10,6 +10,23 @@ import os.log
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
 @objc public class AdMobAdapterConfiguration: NSObject {
 
+    /// The version of the partner SDK.
+    @objc static var partnerSDKVersion: String {
+        let versionNumber = GADMobileAds.sharedInstance().versionNumber
+        return "\(versionNumber.majorVersion).\(versionNumber.minorVersion).\(versionNumber.patchVersion)"
+    }
+
+    /// The version of the adapter.
+    /// It should have either 5 or 6 digits separated by periods, where the first digit is Chartboost Mediation SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
+    /// Format: `<Chartboost Mediation major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
+    @objc static let adapterVersion = "4.11.2.0.0"
+
+    /// The partner's unique identifier.
+    @objc static let partnerID = "admob"
+
+    /// The human-friendly partner name.
+    @objc static let partnerDisplayName = "AdMob"
+
     private static let log = OSLog(subsystem: "com.chartboost.mediation.adapter.admob", category: "Configuration")
 
     /// Google's identifier for your test device can be found in the console output from their SDK
